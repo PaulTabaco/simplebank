a515744f95a2ec42d3cbc4de9a424b175ae07267
Paul Tabaco <rabochiy.delta@gmail.com>
2022-10-30 15:05:52 +0200

    - Added Testify lib - CreateAccount func covered by test WITH MANUAL DATA.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,27 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Vasya",
+		Balance:  100,
+		Currency: "USD",
+	}
 
+	accont, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, accont)
+
+	require.Equal(t, arg.Owner, accont.Owner)
+	require.Equal(t, arg.Balance, accont.Balance)
+	require.Equal(t, arg.Currency, accont.Currency)
+
+	require.NotZero(t, accont.ID)
+	require.NotZero(t, accont.CreatedAt)
 }
