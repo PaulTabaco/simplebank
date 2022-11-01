80dca47d01bbaf69a75aefe537a80b5cb81dbab0
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-01 18:44:41 +0200

    Fix for TranferTxResult model

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -53,7 +53,7 @@ type TransferTxParams struct {
 type TransferTxResult struct {
 	Transfer    Transfer `json:"transfer"`
 	FromAccount Account  `json:"from_account"`
-	ToAccount   Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
 	FromEntry   Entry    `json:"from_entry"`
 	ToEntry     Entry    `json:"to_entry"`
 }
