ca3c78cfe45fd023a3bd230dccd55b026975487b
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-13 03:44:01 +0200

    - fix config file name (gonfig to config) - TOKEN_SYMMETRIC, ACCESS_TOKEN_DURATION params added to app.env file - TokenSymmetricKey, AccessTokenDuration setting added to config that reads from ENV by viper - NewServer now gets config as new parameter; and returns err to addition to Server; - NewServer call changed at main.go
    - NewTestServer added to nain_test.go to provide testServer with config that used test TokenSymmetricKey and test AccessTokenDuration;
    - account_test and user_test updated with new NewTestServer;
    - transfer_test created with all tests;

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,23 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	db "parus.i234.me/paultabaco/simplebank/db/sqlc"
+	"parus.i234.me/paultabaco/simplebank/util"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
