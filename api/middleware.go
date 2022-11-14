e3fa69fc12f3feaa5c1b33f2be1fd291e8cd04f3
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-15 00:39:32 +0200

    - Auth middleware added; (not registered at server.go) - Tests for auth middleware added;

---
+++ api/middleware.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+
+	"parus.i234.me/paultabaco/simplebank/token"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		athorizationHeader := ctx.GetHeader(authorizationHeaderKey)
+		if len(athorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(athorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		athorizationType := strings.ToLower(fields[0])
+		if athorizationType != authorizationTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", athorizationType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+		// if all prev is ok, second element is token
+		accessToken := fields[1]
+
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			err := fmt.Errorf("unsupported authorization type %s", athorizationType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authorizationPayloadKey, payload)
+		ctx.Next() // middleware done and now continue with main hendler
+	}
+}
