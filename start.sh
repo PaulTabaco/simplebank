7a4c9380921b9fad56d29aedea58e51a063e302e
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-21 14:44:41 +0200

    - start.sh updated to apply environments from app.env when app starts in docker; - deploy to aws production yaml added (template only) - useful info files added to source folder - For now it is end. Because next tutorial about AWS using. Not actual for nearest time.

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo "run db migration"
+# load environments from file to current shell
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the simplebank app"
